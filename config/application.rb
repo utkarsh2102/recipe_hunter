d14c57f35b32dd0a9632b5c689ded2d068b5efd8
Utkarsh Gupta <utkarsh@debian.org>
2020-07-27 21:39:45 +0530

    Don't use mocks!
    Signed-off-by: Utkarsh Gupta <utkarsh@debian.org>

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module RecipePuppyBootstrap
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
-    config.use_mocks_please = true
+    config.use_mocks_please = false
   end
 end
